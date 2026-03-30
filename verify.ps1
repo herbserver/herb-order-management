d3187690085b04c5ba991efb0b7ebb0d84a5153a
herbserver <api.herbonnaturals@gmail.com>
2026-03-30 17:20:27 +0530

    final production

--- verify.ps1
+++
@@ -1,6 +0,0 @@
-$t = [System.IO.File]::ReadAllText("public\admin.html", [System.Text.Encoding]::UTF8)
-$l = $t -split "`n"
-Write-Host "Lines: $($l.Count)"
-Write-Host "Line 22: $($l[21])"
-Write-Host "Line 55: $($l[54])"
-Write-Host "Bytes: $([System.IO.File]::ReadAllBytes("public\admin.html").Length)"
